936049b55857151420633a8a1d985fe99856f6fc
liheng <e0320674@u.nus.edu>
2019-02-27 01:53:29 +0800

    added bootstrap and base page

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,31 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # this is the logic how we want to handle when people go to our homepage, we need to map the url and function at urls.py in the project
 def home(request):
     return render(request, 'mysite/home.html')
 
+
+members = [
+    {
+        'name': 'Gao Jiaxue',
+        'chinese_name': '高佳雪'
+    },
+    {
+        'name': 'Teh Li Heng',
+        'chinese_name': '郑理衡',
+        'nickname':'Henry',
+    },
+    {
+        'name': 'Wang Yafeng',
+        'chinese_name': '王亚凤'
+    },
+]
+
+
 def about(request):
-    # return HttpResponse('<h1>Blog About</h1>')
-    return render(request, 'mysite/about.html')
+    # items must be passed as dictionary into context
+    context = {
+        'members': members
+    }
+    return render(request, 'mysite/about.html', context)
