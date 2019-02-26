715d987450106b1a09f4485ad0cace003e628e93
liheng <e0320674@u.nus.edu>
2019-02-27 00:54:28 +0800

    returning with templates

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # this is the logic how we want to handle when people go to our homepage, we need to map the url and function at urls.py in the project
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'mysite/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    # return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'mysite/about.html')
