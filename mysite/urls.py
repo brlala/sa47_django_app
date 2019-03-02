0644104af5f65b80a7174a0af3fc2c6a16af5dc5
Wang yafeng <yafeng618@gmail.com>
2019-03-02 14:59:56 +0800

    accept changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
+from .views import PostListView
 from . import views
 
 # import all views to this url
 
 # use unique naming convention as it may affect when we want to reroute
 urlpatterns = [
-    path('', views.home, name='mysite-home'),
+    # path('', views.home, name='mysite-home'),
+    path('', PostListView.as_view(), name='mysite-home'),
     path('about/', views.about, name='mysite-about'),
 ]
